9c58b99ac935e7a5dab36e4a9df9a5714331bf65
0mar99 <omarag9099@gmail.com>
2025-04-04 13:51:34 -0600

    tundra: Pixel-ify!
    Signed-off-by: 0mar99 <omarag9099@gmail.com>

--- aosp_tundra.mk
+++ aosp_tundra.mk
@@ -11,10 +11,24 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from tundra device
 $(call inherit-product, device/motorola/tundra/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Pixel Project stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_tundra
+# Pixel Project flags
+CUSTOM_BUILDTYPE := UNOFFICIAL
+CUSTOM_MAINTAINER := 0mar
+
+# Gapps Flag
+WITH_GMS := true
+
+# Extras
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_ENABLE_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+
+PRODUCT_NAME := aosp_tundra
 PRODUCT_DEVICE := tundra
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_BRAND := motorola
