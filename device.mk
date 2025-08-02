43fef77f1cb24aa47ded46b86ab6bcd3271c9e04
0mar99 <omarag9099@gmail.com>
2025-08-02 13:30:31 -0600

    tundra: Voltage-ify!
    Signed-off-by: 0mar99 <omarag9099@gmail.com>

--- device.mk
+++ device.mk
@@ -37,6 +37,10 @@ PRODUCT_PACKAGES += \
     SystemUIResDevice \
     WifiResDevice
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-voltage
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/audio_platform_info.xml \
--- voltage_tundra.mk
+++ voltage_tundra.mk
@@ -11,10 +11,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from tundra device
 $(call inherit-product, device/motorola/tundra/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common VoltageOS stuff.
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_tundra
+# VoltageOS flags
+VOLTAGE_BUILD_TYPE := UNOFFICIAL
+
+# Gapps Flag
+WITH_GMS := false
+
+# Extras
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_ENABLE_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+
+PRODUCT_NAME := voltage_tundra
 PRODUCT_DEVICE := tundra
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_BRAND := motorola
